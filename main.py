136f6642bb013d11736f81898b0f34128ba2ca9d
Rijan Dahal <70582929+enkoki@users.noreply.github.com>
2026-04-21 21:21:34 +0545

    refactor: remove class and using function decorators

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import discord
 from config import BOT_TOKEN
 
-class Client(discord.Client):
-    async def on_ready(self):
-        print(f'Successfull! Logged on as {self.user}!')
-        
-
 intents = discord.Intents.default()
 intents.message_content = True
 
+bot = discord.Client(intents=intents)
 
-client = Client(intents=intents)
-client.run(BOT_TOKEN)
+@bot.event
+async def on_ready():
+    print(f"Logged In Successfully! Online as: {bot.user}")
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    if message.content.startswith('bye'):
+        await message.channel.send(f"Goodbye, {message.author}")
+
+if __name__ == '__main__':
+    bot.run(BOT_TOKEN)
