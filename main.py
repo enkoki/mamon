6d9c8f00d5adffb7d03f583cb3fa7cd7602fad26
Rijan Dahal <70582929+enkoki@users.noreply.github.com>
2026-04-22 22:37:59 +0545

    feat: enabled slash commands

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import discord
+from discord.ext import commands
+from discord import app_commands 
 from config import BOT_TOKEN
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix=";", intents=intents)
+GUILD_ID = discord.Object(id=1063511309477494844)
 
 @bot.event
 async def on_ready():
     print(f"Logged In Successfully! Online as: {bot.user}")
+    try:
+        synced = await bot.tree.sync(guild=GUILD_ID)
+        print(f'Synced {len(synced)} commands to guild {GUILD_ID.id}')
+    except Exception as e:
+        print(f'Error syncing commands: {e}')
 
 @bot.event
 async def on_message(message):
@@ -17,5 +25,13 @@ async def on_message(message):
     if message.content.startswith('bye'):
         await message.channel.send(f"Goodbye, {message.author}")
 
+@bot.tree.command(name="hello", description="Say Hello!", guild=GUILD_ID)
+async def say_hello(interaction: discord.Interaction):
+    await interaction.response.send_message("Hi there!")
+    
+@bot.tree.command(name="say", description="Repeat the given words.", guild=GUILD_ID)    
+async def say(interaction: discord.Interaction, string: str):
+    await interaction.response.send_message(f'{string}')
+    
 if __name__ == '__main__':
     bot.run(BOT_TOKEN)
