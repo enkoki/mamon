ce48d2e0c3ef902bd2238527e9a3528da1fa9faa
Rijan Dahal <reasondahal719@gmail.com>
2026-04-23 13:03:25 +0545

    feat: add slash command with embed response

--- config.py
+++ config.py
@@ -9,3 +9,4 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 # Others
 ID = os.getenv("GUILD_ID")
 GUILD_ID = discord.Object(id=ID)
+OWNER_ID = os.getenv("OWNER_ID")
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import discord
 from discord.ext import commands
 from discord import app_commands 
-from config import BOT_TOKEN, GUILD_ID
+from config import BOT_TOKEN, GUILD_ID, OWNER_ID
+
+from datetime import datetime
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -34,6 +36,50 @@ async def say_hello(interaction: discord.Interaction):
 @bot.tree.command(name="say", description="Repeat the given words.", guild=GUILD_ID)    
 async def say(interaction: discord.Interaction, string: str):
     await interaction.response.send_message(f'{string}')
-    
+
+@bot.tree.command(name="github", description="The Github Repository of the Bot..", guild=GUILD_ID)    
+async def github(interaction: discord.Interaction):
+    owner = await bot.fetch_user(OWNER_ID)    
+    embed = discord.Embed(
+        title="GitHub Repository | Mamon",
+        url="https://github.com/enkoki/mamon",
+        description=(
+            "A Discord bot built using discord.py, created for learning the basics of bot development. "
+            "This project is currently in early development and is mainly focused on learning and experimenting with Discord bot fundamentals."
+        ),
+        color=discord.Color.from_str("#000000")
+    )
+
+    embed.set_thumbnail(url="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/github-white-icon.png")
+
+    embed.add_field(
+        name="Commands",
+        value=(
+            "`/hello`\n"
+            "`/say <text>`\n"
+            "`/github`\n"
+        ),
+        inline = False
+    )
+
+    embed.add_field(
+        name="Features",
+        value=(
+            "- Leveling\n"
+            "- Moderation\n"
+            "- Logs"
+        )
+    )
+
+    embed.set_footer(text="Make sure to Star the Repository!")
+    embed.set_author(
+        name=owner.name,
+        icon_url=owner.display_avatar.url
+    )
+    embed.set_image(url="https://i.imgur.com/IBrUrab.png")
+    embed.timestamp = datetime.now()
+
+    await interaction.response.send_message(embed=embed)
+
 if __name__ == '__main__':
     bot.run(BOT_TOKEN)
