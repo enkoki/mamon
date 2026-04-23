633bfcb03b846d1a8f40638d09c3a86253bc68c8
Rijan Dahal <reasondahal719@gmail.com>
2026-04-23 14:10:04 +0545

    feat: add logging and rich logging

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import asyncio
-
 import discord
 from discord.ext import commands
 import config
+
+from rich.logging import RichHandler
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(message)s",
+    datefmt="%H:%M:%S",
+    handlers=[RichHandler()]
+)
+# # If you want to remove the warning related to PyNaCl and davey
+# logging.getLogger("discord").setLevel(logging.ERROR) 
+
+log = logging.getLogger(__name__)
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -13,14 +26,18 @@ bot = commands.Bot(command_prefix=";", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f"Logged In Successfully! Online as: {bot.user}")
+    log.info(f"Logged in successfully! Online as {bot.user}")
 
 async def main():
     async with bot:
-        print("Loading cogs...")
+        log.info("Loading cogs...")
         for cog in cogs:
-            await bot.load_extension(cog)
-        print("Starting the bot...")
+            try:
+                await bot.load_extension(cog)
+                log.info(f"Loaded {cog}")
+            except Exception as e:
+                log.error(f"Failed to load {cog}: {e}")
+        log.info("Starting the bot...")
         await bot.start(config.BOT_TOKEN)
 
 asyncio.run(main())
